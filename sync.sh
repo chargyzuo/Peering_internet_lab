3b4ffaa11b856c0e2359f53db11d67d1bd451929
chargyzuo <779260457@qq.com>
2026-08-24 21:38:43 +0800

    change lab MGM_linux

--- sync.sh
+++ sync.sh
@@ -6,4 +6,4 @@ rsync -e 'ssh -J root@192.168.192.51' -av --delete --exclude='.git' \
   --exclude='venv/' \
   --exclude='__pycache__/' \
   --exclude='*.pyc' \
-  ./ root@10.99.99.2:~/Peering_internet_lab/
+  ./ root@10.99.99.4:~/Peering_internet_lab/
